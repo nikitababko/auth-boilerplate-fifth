0ec7e2d083aa0a8e74de902fb64aa19c04e6089e
Nikita Babko <niki-babko@yandex.ru>
2021-03-25 10:12:02 +0700

    Added main file.

---
+++ server/src/index.js
@@ -0,0 +1,40 @@
+require('dotenv').config({
+  path: 'src/config/keys.env',
+});
+
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+
+const connectDB = require('./core/db');
+const createRoutes = require('./core/routes');
+
+// App init
+const app = express();
+
+// Setup app
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+// Register routes
+createRoutes(app);
+
+// Connect to DB
+connectDB();
+
+// Setup server
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  try {
+    console.log(`Server up and running on port: ${PORT}`);
+  } catch (error) {
+    console.error(error);
+  }
+});
