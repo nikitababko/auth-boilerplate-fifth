61e326bb26f38e60334c30d5d67c78c86567658a
Nikita Babko <niki-babko@yandex.ru>
2021-03-27 13:57:30 +0700

    Added routes: forgot, reset and info.

--- server/src/core/routes.js
+++ server/src/core/routes.js
@@ -1,7 +1,14 @@
 const UserController = require('../controllers/UserController');
+const auth = require('../middlewares/auth');
 
 const createRoutes = (app) => {
   app.post('/user/register', UserController.register);
+  app.post('/user/activation', UserController.activateEmail);
+  app.post('/user/login', UserController.login);
+  app.post('/user/refresh_token', UserController.getAccessToken);
+  app.post('/user/forgot', UserController.forgotPassword);
+  app.post('/user/reset', auth, UserController.resetPassword);
+  app.get('/user/info', auth, UserController.getUserInfo);
 };
 
 module.exports = createRoutes;
