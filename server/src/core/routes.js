d8977aeca9c96d06c6e8eda804d44537842772ec
Nikita Babko <niki-babko@yandex.ru>
2021-03-28 14:53:11 +0700

    Added updateUser method.

--- server/src/core/routes.js
+++ server/src/core/routes.js
@@ -6,12 +6,16 @@ const createRoutes = (app) => {
   app.post('/user/register', UserController.register);
   app.post('/user/activation', UserController.activateEmail);
   app.post('/user/login', UserController.login);
+  app.get('/user/logout', UserController.logout);
   app.post('/user/refresh_token', UserController.getAccessToken);
+
   app.post('/user/forgot', UserController.forgotPassword);
   app.post('/user/reset', auth, UserController.resetPassword);
+
   app.get('/user/info', auth, UserController.getUserInfo);
   app.get('/user/all_info', auth, authAdmin, UserController.getUsersAllInfo);
-  app.get('/user/logout', UserController.logout);
+
+  app.patch('/user/update', auth, UserController.updateUser);
 };
 
 module.exports = createRoutes;
