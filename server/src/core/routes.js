394b8cbd88f412324775a1fbe7cd4ac755de0e2c
Nikita Babko <niki-babko@yandex.ru>
2021-03-28 16:26:50 +0700

    Added upload route.

--- server/src/core/routes.js
+++ server/src/core/routes.js
@@ -1,6 +1,8 @@
+const UploadController = require('../controllers/UploadController');
 const UserController = require('../controllers/UserController');
 const auth = require('../middlewares/auth');
 const authAdmin = require('../middlewares/authAdmin');
+const uploadImage = require('../middlewares/uploadImage');
 
 const createRoutes = (app) => {
   app.post('/user/register', UserController.register);
@@ -16,7 +18,10 @@ const createRoutes = (app) => {
   app.get('/user/all_info', auth, authAdmin, UserController.getUsersAllInfo);
 
   app.patch('/user/update', auth, UserController.updateUser);
+  app.post('/api/upload_avatar', uploadImage, auth, UploadController.uploadAvatar);
   app.patch('/user/update_role/:id', auth, authAdmin, UserController.updateUserRole);
+
+  app.delete('/user/delete/:id', auth, authAdmin, UserController.deleteUser);
 };
 
 module.exports = createRoutes;
