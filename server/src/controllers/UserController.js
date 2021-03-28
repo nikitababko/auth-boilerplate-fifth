d0e232d889e788943811fafc8076f43ef1b806bb
Nikita Babko <niki-babko@yandex.ru>
2021-03-28 14:53:05 +0700

    Added updateUser method.

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -226,6 +226,24 @@ const UserController = {
       return res.status(500).json({ msg: err.message });
     }
   },
+
+  // User update
+  updateUser: async (req, res) => {
+    try {
+      const { name, avatar } = req.body;
+      await UserModel.findOneAndUpdate(
+        { _id: req.user.id },
+        {
+          name,
+          avatar,
+        }
+      );
+
+      res.json({ msg: 'Update Success!' });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
 };
 
 module.exports = UserController;
