e3ff0e64f4fc4e2fa420b8de9964007d36eb5f17
Nikita Babko <niki-babko@yandex.ru>
2021-03-30 20:55:31 +0700

    Added opportunity token activation.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
 
+import { dispatchLogin, fetchUser, dispatchGetUser } from 'redux/actions/authAction';
 import { Header } from './components';
 import Body from './pages/Body';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.token);
+  const auth = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    const firstLogin = localStorage.getItem('firstLogin');
+    if (firstLogin) {
+      const getToken = async () => {
+        const res = await axios.post('/user/refresh_token', null);
+        dispatch({ type: 'GET_TOKEN', payload: res.data.access_token });
+      };
+      getToken();
+    }
+  }, [auth.isLogged, dispatch]);
+
+  useEffect(() => {
+    if (token) {
+      const getUser = () => {
+        dispatch(dispatchLogin());
+
+        return fetchUser(token).then((res) => {
+          dispatch(dispatchGetUser(res));
+        });
+      };
+      getUser();
+    }
+  }, [token, dispatch]);
+
   return (
-    <div className="app">
-      <Router>
+    <Router>
+      <div className="App">
         <Header />
         <Body />
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 };
 
