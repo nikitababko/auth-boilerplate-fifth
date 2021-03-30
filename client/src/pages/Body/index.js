673c6939c0b5039489cfba37f5d18da4d294f55c
Nikita Babko <niki-babko@yandex.ru>
2021-03-30 20:56:33 +0700

    Exported state with useSelector.

--- client/src/pages/Body/index.js
+++ client/src/pages/Body/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import { Login, Register } from 'pages/Auth';
-import { ActivationEmail } from 'components';
+import { ActivationEmail, NotFound } from 'components';
 
 const Body = () => {
+  const auth = useSelector((state) => state.auth);
+  const { isLogged } = auth;
+
   return (
     <section>
       <Switch>
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={isLogged ? NotFound : Login} />
+        <Route exact path="/register" component={isLogged ? NotFound : Register} />
         <Route
           exact
           path="/user/activate/:activation_token"
