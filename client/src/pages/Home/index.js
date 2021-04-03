0057f9097fccf3c24367ef6adecc5c07f4eb960b
Nikita Babko <niki-babko@yandex.ru>
2021-04-03 19:40:10 +0700

    Added home page.

---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.scss';
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <h2>Hello everyone!</h2>
+    </div>
+  );
+};
+
+export default Home;
