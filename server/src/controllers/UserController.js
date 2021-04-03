20199cb4836cd12acd1e2e220c35fcb6875ea460
Nikita Babko <niki-babko@yandex.ru>
2021-04-03 19:38:22 +0700

    Added opportunity auth with facebook.

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -365,6 +365,65 @@ const UserController = {
       return res.status(500).json({ msg: err.message });
     }
   },
+
+  // Facebook login
+  facebookLogin: async (req, res) => {
+    try {
+      const { accessToken, userID } = req.body;
+
+      const URL = `https://graph.facebook.com/v2.9/${userID}/?fields=id,name,email,picture&access_token=${accessToken}`;
+
+      const data = await fetch(URL)
+        .then((res) => res.json())
+        .then((res) => {
+          return res;
+        });
+
+      const { email, name, picture } = data;
+
+      const password = email + process.env.FACEBOOK_SECRET;
+
+      const passwordHash = await bcrypt.hash(password, 12);
+
+      const user = await UserModel.findOne({ email });
+
+      if (user) {
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+          return res.status(400).json({ msg: 'Password is incorrect.' });
+        }
+
+        const refresh_token = createRefreshToken({ id: user._id });
+        res.cookie('refreshtoken', refresh_token, {
+          httpOnly: true,
+          path: '/user/refresh_token',
+          maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        });
+
+        res.json({ msg: 'Login success!' });
+      } else {
+        const newUser = new UserModel({
+          name,
+          email,
+          password: passwordHash,
+          avatar: picture.data.url,
+        });
+
+        await newUser.save();
+
+        const refresh_token = createRefreshToken({ id: newUser._id });
+        res.cookie('refreshtoken', refresh_token, {
+          httpOnly: true,
+          path: '/user/refresh_token',
+          maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        });
+
+        res.json({ msg: 'Login success!' });
+      }
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
 };
 
 module.exports = UserController;
